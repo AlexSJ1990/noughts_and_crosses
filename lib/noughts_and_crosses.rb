0f64b7610725595c0ff8c77953183dda053d6218
Alex SinclairJoice <alex.sinclair.joice@gmail.com>
2018-09-28 15:01:13 +0100

    method to ask if player wants to play

--- lib/noughts_and_crosses.rb
+++ lib/noughts_and_crosses.rb
@@ -2,4 +2,18 @@ require "noughts_and_crosses/version"
 
 module NoughtsAndCrosses
   # Your code goes here...
+
+  class Welcome
+    def play?
+      puts "Hello, would you like to play a game of Noughts and Crosses Y or N?"
+      answer = gets.chomp.downcase
+      if answer == "y"
+        #play game method
+      elsif answer == "n"
+        puts "Okay, see you next time!"
+      else
+        puts "Please select Y or N"
+      end
+    end
+  end
 end
